2f9c8ee5e75cd91feda9f781edeeca913b2a63de
ephyg <ephywaanofii@gmail.com>
2024-07-29 21:01:37 +0300

    feat: Update item list in Todo List application

--- task-1/main.js
+++ task-1/main.js
@@ -4,8 +4,8 @@ let itemsList = document.getElementById("items");
 let inputField = document.getElementById("input-field");
 let addButton = document.getElementById("add-button");
 
-let items = ["Apple","Orange"];
-
+let items = ["Apple", "Banana", "Mango", "Orange", "Pineapple"];
+let taskTitle = document.getElementById("task-title")
 // show items
 function showItems() {
   itemsList.innerHTML = "";
@@ -13,27 +13,57 @@ function showItems() {
     itemsList.innerHTML += `<li id="item">
         <span id="item-number">${i + 1} </span>
         <span id="item-name">${items[i]}</span>
-        <button id="delete-button">Delete</button>
+        <div class="">
+          <button id="edit-button" class="edit-button">Edit</button>
+          <button id="delete-button" class="delete-button">Delete</button
+
+        </div>
       </li>`;
   }
 }
 showItems();
 
-// delete item
+
+
+// edit item
+let editIndex = null;
 itemsList.addEventListener("click", function (e) {
-  if (e.target.id === "delete-button") {
-    // console.log(e.target.parentElement.children[0].textContent);
-    items.splice(e.target.parentElement.children[0].textContent - 1, 1);
-    showItems();
+  if (e.target.id === "edit-button") {
+    let index = e.target.parentElement.parentElement.children[0];
+    editIndex = index.textContent - 1;
+    inputField.value = items[editIndex];
+    addButton.innerHTML = "Update";
+    taskTitle.innerHTML = "Edit Item"
+
   }
 });
-
 
 // add items
 addButton.addEventListener("click", function () {
   if (inputField.value !== "") {
-    items.push(inputField.value);
+    if (editIndex !== null) {
+      items[editIndex] = inputField.value;
+      editIndex = null;
+      addButton.innerHTML = "Add";
+    } else {
+      items.push(inputField.value);
+    }
     showItems();
     inputField.value = "";
+  }
+});
+
+
+// delete item
+itemsList.addEventListener("click", function (e) {
+  console.log(e.target.id);
+  if (e.target.id === "delete-button") {
+    console.log(e.target.parentElement.parentElement.children[0].textContent);
+    items.splice(
+      e.target.parentElement.parentElement.children[0].textContent - 1,
+      1
+    );
+    showItems();
+    editIndex = null
   }
 });
