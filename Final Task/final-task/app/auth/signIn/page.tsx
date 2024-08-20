195801940956716dbd1b37932bdf41e2ca5e8ccd
ephyg <84809084+ephyg@users.noreply.github.com1>
2024-08-20 18:06:47 +0300

    feat: Add lazy loading and image for login page

--- Final Task/final-task/app/auth/signIn/page.tsx
+++ Final Task/final-task/app/auth/signIn/page.tsx
@@ -1,12 +1,14 @@
 import LoginForm from "@/components/Forms/loginForm";
 import { SessionProvider } from "next-auth/react";
+import Image from "next/image";
 import Link from "next/link";
 
 const Login = () => {
-
   return (
     <div className="flex min-h-screen max-w-7xl mx-auto">
-      <div className="flex-1 flex"></div>
+      <div className="flex-1 flex relative">
+        <Image src="/login-image.png" alt="" fill />
+      </div>
       <div className=" flex-1 flex flex-col justify-center items-center w-full gap-3">
         <div className="flex flex-col items-center justify-center gap-6 px-28 w-full">
           <h1 className="font-epilogue900 text-headertext1 text-black">
